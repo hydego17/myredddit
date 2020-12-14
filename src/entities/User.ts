4f3cfb4910c6b834a948bedb5ba5495ae0097473
Umma Ahimsha <uahimsha@gmail.com>
2020-12-14 15:23:47 +0700

    added register route

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { IsEmail, Length } from "class-validator";
+import bcrypt from "bcrypt";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+} from "typeorm";
+import { classToPlain, Exclude } from "class-transformer";
 
 @Entity("users")
 export class User extends BaseEntity {
@@ -6,15 +18,38 @@ export class User extends BaseEntity {
     super();
     Object.assign(this, user);
   }
+
+  @Exclude()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Index()
+  @IsEmail()
   @Column({ unique: true })
   email: string;
 
+  @Index()
+  @Length(3, 255)
   @Column({ unique: true })
   username: string;
 
+  @Exclude()
   @Column()
+  @Length(6, 255)
   password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import morgan from "morgan";
 import { createConnection } from "typeorm";
 
 import authRoutes from "./routes/auth";
+import trim from "./middlewares/trim";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(trim);
 
 app.get("/", (_, res) => res.send("Hellowww"));
 app.use("/api/auth", authRoutes);
