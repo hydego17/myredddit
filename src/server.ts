8fbb51d05200a2ce58706c15b3121a46901aa766
Umma Ahimsha <uahimsha@gmail.com>
2020-12-14 18:12:02 +0700

    init

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,13 @@ import cookieParser from "cookie-parser";
 import { createConnection } from "typeorm";
 
 import authRoutes from "./routes/auth";
+import postRoutes from "./routes/post";
 import trim from "./middlewares/trim";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -20,9 +22,10 @@ app.use(cookieParser());
 
 app.get("/", (_, res) => res.send("Hellowww"));
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
-app.listen(5000, async () => {
-  console.log("Server running at http://localhost:5000");
+app.listen(PORT, async () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 
   try {
     await createConnection();
